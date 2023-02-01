c0640e69c81da6bafc04f21147d42a273572cdf6
karagyozovLT <georgi.karagyozov@looming.tech>
2023-02-01 23:39:04 +0200

    refactor

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div>
+    <>
       <div className="flex items-center justify-center min-h-screen p-5 bg-gray-100 min-w-screen">
         <div className="flex space-x-2 animate-pulse">
           <div className="w-4 h-4 bg-gray-500 rounded-full" />
@@ -10,6 +10,6 @@ export default function Loading() {
           <div className="w-4 h-4 bg-gray-500 rounded-full" />
         </div>
       </div>
-    </div>
+    </>
   );
 }
